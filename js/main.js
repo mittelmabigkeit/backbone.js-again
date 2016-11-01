30cde7055313abb9f6275a0a9501b7f30ac3c2f4
mittelmabigkeit <comp422012016@mail.ru>
2016-11-01 17:13:53 +0300

    decl

--- js/main.js
+++ js/main.js
@@ -11,18 +11,35 @@ var Person = Backbone.Model.extend({
     }
 });
 
+//var PersonView = Backbone.View.extend({
+//    tagName: 'div',
+//    className: 'TOP',
+//    id: 'STAN',
+//    render: function () {
+//        this.$el.html(this.model.get('words'));
+//    }
+//});
+//
+//var person = new Person;
+//var personView = new PersonView({model: person});
+
 var PersonView = Backbone.View.extend({
-    tagName: 'div',
-    className: 'TOP',
-    id: 'STAN',
-    render: function () {
-        this.$el.html(this.model.get('words'));
+    tagName: 'li',
+
+    template: _.template('<%=name%> (<%=age%>) - <%=job%>'),
+
+    initialize: function() {
+        this.render();
+    },
+
+    render: function() {
+        //this.$el.html(this.model.get('name') + ' (' + this.model.get('age') + ') - ' + this.model.get('job'));
+        this.$el.html(this.template(this.model.toJSON()));
     }
 });
 
 var person = new Person;
 var personView = new PersonView({model: person});
-
 
 //CONSOLE COMMANDS
 //personView;
